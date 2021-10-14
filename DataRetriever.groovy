26f3ea543865a0344e73030237f9d806efdd4009
Karolina Wojtowicz <karolina.wojtowicz@infor.com>
2021-10-14 12:21:01 +0200

    Validate string and integer methods

--- DataRetriever.groovy
+++ DataRetriever.groovy
@@ -11,27 +11,27 @@ public class DataRetriveUtil extends ExtendM3Utility {
         }
         .orElseThrow( () -> new IsNullExeption("Is null"));
 
-    public Optional<String> ofNullable validateInput(MIAPI, String getField){
+    public Optional<String> ofNullable validateStringType(MIAPI, String getField){
         
-        if (mi.inData(fieldName).isBlank()) || (mi.inData(fieldName).equals(0)){
-            return Optional.of()
+        if (mi.inData(fieldName).isBlank() || mi.inData(fieldName).equals("?")){
+            return Optional.of(String "")
         }
-
-        for (mi.inData(fieldName).equals("?")){
-            if (mi.inData(fieldName)instanceof String){
-                return Optional.of(String "")
-            }
-            else {
-                return Optional.of(Integer "0")
-            }
-        }
-        if (mi.inData(fieldName) instanceof String) {
+        else{
             return Optional.of(String mi.inData(fieldName))
         }
-
     }
 
-    public boolean isDateValid(int dateNum) {
+    public Optional<String> ofNullable validateNumericalType(MIAPI, String getField){
+        int numField = Integer.parseInt(getField)
+        if (mi.inData(fieldName).isBlank() || mi.inData(fieldName).equals("?")){
+            return Optional.of(Integer "0")
+        }
+        else{
+            return Optional.of(Integer mi.inData(fieldName))
+        }
+    }
+
+    public boolean isDateValid(String mi.inData(fieldName)) {
         String date = dateNum.toString();
         try {
         LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyymmdd"));
